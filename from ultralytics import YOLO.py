f3d0711ffa62390aea3037a18e8a0304847684cd
malakmohammedalyaremi <memomalaky267@gmail.com>
2025-02-18 22:14:05 -0800

    Delete from ultralytics import YOLO.py

--- from ultralytics import YOLO.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO ("yolo11s.pt")
-
-results = model.predict(source=r"C:\Users\USER\OneDrive\Pictures\lion.jpg")
-        
-results [0].show() 
-                        
-                        
